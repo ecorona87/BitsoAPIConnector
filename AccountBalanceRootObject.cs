7c2b5711efb144f42687507df0b2da0ee6a9cbbd
ecorona87 <nemesis2040@hotmail.com>
2018-01-02 18:10:30 -0600

    Initial commit

---
+++ AccountBalanceRootObject.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitsoConnectorLibrary
+{
+    public class Balance
+    {
+        public string currency { get; set; }
+        public string total { get; set; }
+        public string locked { get; set; }
+        public string available { get; set; }
+    }
+
+    public class AccountBalancePayload
+    {
+        public List<Balance> balances { get; set; }
+    }
+
+    public class AccountBalanceRootObject
+    {
+        public bool success { get; set; }
+        public AccountBalancePayload payload { get; set; }
+    }
+}
---
+++ FeesRootObject.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitsoConnectorLibrary
+{
+    public class Fee
+    {
+        public string book { get; set; }
+        public string fee_decimal { get; set; }
+        public string fee_percent { get; set; }
+    }
+
+    public class WithdrawalFees
+    {
+        public string btc { get; set; }
+        public string eth { get; set; }
+    }
+
+    public class FeesPayload
+    {
+        public List<Fee> fees { get; set; }
+        public WithdrawalFees withdrawal_fees { get; set; }
+    }
+
+    public class FeesRootObject
+    {
+        public bool success { get; set; }
+        public FeesPayload payload { get; set; }
+    }
+}
